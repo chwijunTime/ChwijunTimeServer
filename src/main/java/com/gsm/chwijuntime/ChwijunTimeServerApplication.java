e55493646eeab17bca90763239a02aede24115ed
김상현 <llmm030506@naver.com>
2021-03-23 07:56:57 +0900

    Initial commit

---
+++ src/main/java/com/gsm/chwijuntime/ChwijunTimeServerApplication.java
@@ -0,0 +1,13 @@
+package com.gsm.chwijuntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChwijunTimeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChwijunTimeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsm/chwijuntime/ChwijunTimeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm.chwijuntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChwijunTimeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
