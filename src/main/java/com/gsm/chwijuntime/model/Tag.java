b37df37f1f3e7024155ff88a4a1a1301fc2599ad
김상현 <llmm030506@naver.com>
2021-03-24 17:56:58 +0900

    Tag table design and association setting & honey tip tag mapping table design

--- src/main/java/com/gsm/chwijuntime/model/Tag.java
+++ src/main/java/com/gsm/chwijuntime/model/Tag.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,26 +13,18 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TipsStorage {
+public class Tag {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long TipsStorageIdx;
+    private Long TagIdx;
 
     @Column(nullable = false)
-    private String WorkCompanyName;
+    private String TagName;
 
-    @Column(nullable = false)
-    private String WorkCompanyAddress;
-
-    @Column(nullable = false)
-    private String TipsInfo;
-
-    // ============== 외래키(연관관계 주인) ================= //
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "MemberIdx")
-    private Member member;
 
     // ============== 태그 매핑(연관관계 노예) =================== //
     @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY)
     private List<TipsStorageTag> tipsStorageTags = new ArrayList<>();
+
+
 }
