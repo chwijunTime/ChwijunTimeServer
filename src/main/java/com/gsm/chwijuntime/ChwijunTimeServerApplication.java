3b560f608d6dc413a62d9fc44969a56c5f4ed77a
김상현 <llmm030506@naver.com>
2021-05-20 15:03:31 +0900

    fixed untracked files

--- src/main/java/com/gsm/chwijuntime/ChwijunTimeServerApplication.java
+++ src/main/java/com/gsm/chwijuntime/ChwijunTimeServerApplication.java
@@ -2,6 +2,7 @@ package com.gsm.chwijuntime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -9,8 +10,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableJpaAuditing
 public class ChwijunTimeServerApplication {
 
+    String PROPERTIES = "spring.config.location=classpath:/application.yml"
+            +",classpath:/key.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(ChwijunTimeServerApplication.class, args);
+        //SpringApplication.run(ChwijunTimeServerApplication.class, args);
+        new SpringApplicationBuilder(ChwijunTimeServerApplication.class)
+                .properties(PROPERTIES)
+                .run(args);
     }
 
 }
