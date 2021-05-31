ca0c824863cb908cb130469d6c2f3a368e549885
김상현 <llmm030506@naver.com>
2021-05-31 22:12:46 +0900

    tipstorageTest

--- src/test/java/com/gsm/chwijuntime/service/TipStorageServiceTest.java
+++ src/test/java/com/gsm/chwijuntime/service/TipStorageServiceTest.java
@@ -1,12 +1,23 @@
 package com.gsm.chwijuntime.service;
 
+import com.gsm.chwijuntime.dto.member.MemberJoinDto;
+import com.gsm.chwijuntime.dto.member.MemberLoginDto;
+import com.gsm.chwijuntime.dto.tipstorage.TipsStorageResDto;
+import com.gsm.chwijuntime.dto.tipstorage.TipsStorageSaveDto;
+import com.gsm.chwijuntime.model.Tag;
+import com.gsm.chwijuntime.service.member.MemberService;
+import com.gsm.chwijuntime.service.tag.TagService;
+import com.gsm.chwijuntime.service.tipstorage.TipsStorageService;
+import com.gsm.chwijuntime.util.GetUserEmailUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
-import static org.junit.Assert.*;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -17,6 +28,24 @@ import static org.junit.Assert.*;
 @Transactional
 public class TipStorageServiceTest {
 
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    TipsStorageService tipsStorageService;
+    @Autowired
+    TagService tagService;
+    @Autowired
+    GetUserEmailUtil getUserEmailUtil;
 
+    @Test
+    public void 회원가입_팁_저장(){
+        log.info("회원가입 실행");
+        MemberJoinDto memberJoinDto = MemberJoinDto.builder()
+                .memberEmail("llmm030506@gmail.com")
+                .memberPassword("1234")
+                .memberClassNumber("2101")
+                .build();
+        memberService.InsertMember(memberJoinDto);
+    }
 
 }
