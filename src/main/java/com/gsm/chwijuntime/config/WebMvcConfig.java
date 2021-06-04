bc32f1ac7e1d5be8b230257b9cb0acc2f90d591c
김상현 <llmm030506@naver.com>
2021-06-04 10:01:31 +0900

    exposedHeaders

--- src/main/java/com/gsm/chwijuntime/config/WebMvcConfig.java
+++ src/main/java/com/gsm/chwijuntime/config/WebMvcConfig.java
@@ -14,7 +14,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowCredentials(true)
                 .allowedHeaders("*")
-                .exposedHeaders("*")
                 .exposedHeaders("Authorization");	//make client read header("Authorization")
     }
 }
