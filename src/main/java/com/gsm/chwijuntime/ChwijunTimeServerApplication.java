e1148960fb419acfbde1ec89d85375c9cbd671aa
김상현 <llmm030506@naver.com>
2021-06-08 00:13:41 +0900

    DDoS attack defense

--- src/main/java/com/gsm/chwijuntime/ChwijunTimeServerApplication.java
+++ src/main/java/com/gsm/chwijuntime/ChwijunTimeServerApplication.java
@@ -2,10 +2,12 @@ package com.gsm.chwijuntime;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class ChwijunTimeServerApplication {
     public static void main(String[] args) {
         String PROPERTIES = "spring.config.location=classpath:/application.yml"
