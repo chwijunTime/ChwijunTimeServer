e6e613a554bcf1654263a4e4d8b8c2cbb33ef158
김상현 <llmm030506@naver.com>
2021-06-08 14:10:09 +0900

    commit

--- src/main/java/com/gsm/chwijuntime/config/WebMvcConfig.java
+++ src/main/java/com/gsm/chwijuntime/config/WebMvcConfig.java
@@ -3,15 +3,12 @@ package com.gsm.chwijuntime.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
@@ -28,29 +25,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .exposedHeaders("Authorization");	//make client read header("Authorization")
     }
 
-    // Form data
-    @Bean
-    public FilterRegistrationBean getXssEscapeServletFilterRegistrationBean() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(new XssEscapeServletFilter());
-        registrationBean.setOrder(1);
-        registrationBean.addUrlPatterns("/*"); //filter를 거칠 url patterns
-        return registrationBean;
-    }
-
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(escapingConverter());
-
     }
 
     @Bean
     public HttpMessageConverter escapingConverter() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
         MappingJackson2HttpMessageConverter escapingConverter =
                 new MappingJackson2HttpMessageConverter();
         escapingConverter.setObjectMapper(objectMapper);
