46a4ab026cec4f551466b405f70dc038042777bc
김상현 <llmm030506@naver.com>
2021-06-22 15:40:55 +0900

    add @Value

--- src/main/java/com/gsm/chwijuntime/security/PrometheusWebConfig.java
+++ src/main/java/com/gsm/chwijuntime/security/PrometheusWebConfig.java
@@ -2,21 +2,34 @@ package com.gsm.chwijuntime.security;
 
 import com.gsm.chwijuntime.handler.CustomAccessDeniedHandler;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Order(1)
+@Slf4j
 public class PrometheusWebConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+
+    @Value("${spring.prometheus.name}")
+    private String USERNAME;
+
+    @Value("${spring.prometheus.password}")
+    private String PASSWORD;
+
+    @Value("${spring.prometheus.role}")
+    private String USER_ROLE;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -24,7 +37,7 @@ public class PrometheusWebConfig extends WebSecurityConfigurerAdapter {
                 .antMatcher("/actuator/**")
                 .authorizeRequests()
                 .anyRequest()
-                .hasRole("prometheus-user")
+                .hasRole(USER_ROLE)
                 .and()
                 .httpBasic();
     }
@@ -33,8 +46,8 @@ public class PrometheusWebConfig extends WebSecurityConfigurerAdapter {
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
         authenticationManagerBuilder.inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder)
-                .withUser("actuator")
-                .password(passwordEncoder.encode("ksh03050621!"))
-                .roles("prometheus-user");
+                .withUser(USERNAME)
+                .password(passwordEncoder.encode(PASSWORD))
+                .roles(USER_ROLE);
     }
 }
