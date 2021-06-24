584a0f3d281c6b512f6b48e74fcac155ade58584
김상현 <llmm030506@naver.com>
2021-06-24 10:30:36 +0900

    commit

--- src/main/java/com/gsm/chwijuntime/config/WebMvcConfig.java
+++ src/main/java/com/gsm/chwijuntime/config/WebMvcConfig.java
@@ -25,7 +25,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:60100")
                 .allowedMethods("*")
                 .allowCredentials(true)
                 .allowedHeaders("*")
