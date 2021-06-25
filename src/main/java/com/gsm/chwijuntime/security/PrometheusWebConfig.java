5a4dc6b82bcc7f3e9eee3f26b462db67218a1dcf
김상현 <llmm030506@naver.com>
2021-06-25 10:30:41 +0900

    update PrometheusWebConfig

--- src/main/java/com/gsm/chwijuntime/security/PrometheusWebConfig.java
+++ src/main/java/com/gsm/chwijuntime/security/PrometheusWebConfig.java
@@ -1,53 +1,52 @@
-package com.gsm.chwijuntime.security;
-
-import com.gsm.chwijuntime.handler.CustomAccessDeniedHandler;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.parameters.P;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-@Order(1)
-@Slf4j
-public class PrometheusWebConfig extends WebSecurityConfigurerAdapter {
-
-    private final PasswordEncoder passwordEncoder;
-
-    @Value("${spring.prometheus.name}")
-    private String USERNAME;
-
-    @Value("${spring.prometheus.password}")
-    private String PASSWORD;
-
-    @Value("${spring.prometheus.role}")
-    private String USER_ROLE;
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/actuator/**")
-                .authorizeRequests()
-                .anyRequest()
-                .hasRole(USER_ROLE)
-                .and()
-                .httpBasic();
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser(USERNAME)
-                .password(passwordEncoder.encode(PASSWORD))
-                .roles(USER_ROLE);
-    }
-}
+//package com.gsm.chwijuntime.security;
+//
+//import com.gsm.chwijuntime.handler.CustomAccessDeniedHandler;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//@Order(1)
+//@Slf4j
+//public class PrometheusWebConfig extends WebSecurityConfigurerAdapter {
+//
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Value("${spring.prometheus.name}")
+//    private String USERNAME;
+//
+//    @Value("${spring.prometheus.password}")
+//    private String PASSWORD;
+//
+//    @Value("${spring.prometheus.role}")
+//    private String USER_ROLE;
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//                .antMatcher("/actuator/**")
+//                .authorizeRequests()
+//                .anyRequest()
+//                .hasRole(USER_ROLE)
+//                .and()
+//                .httpBasic();
+//    }
+//
+//    @Override
+//    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+//        authenticationManagerBuilder.inMemoryAuthentication()
+//                .passwordEncoder(passwordEncoder)
+//                .withUser(USERNAME)
+//                .password(passwordEncoder.encode(PASSWORD))
+//                .roles(USER_ROLE);
+//    }
+//}
